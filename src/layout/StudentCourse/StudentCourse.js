5da2be970e4495e855598604430989a600d750b9
Roihan <hroihan6@gmail.com>
2022-08-10 13:51:29 +0700

    Added student course layout

--- src/layout/StudentCourse/StudentCourse.js
+++ src/layout/StudentCourse/StudentCourse.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './StudentCourse.module.css';
 import ContentContainer from '../../UI/ContentContainer/ContentContainer';
+import CourseListContainer from '../../UI/CourseListContainer/CourseListContainer';
+import CourseContainer from '../../UI/CourseContainer/CourseContainer';
+
+const VerifiedCourse = (props) => {
+  return <CourseContainer {...props}>
+  </CourseContainer>;
+}
 
 const StudentCourse = () => {
   return (
-    <ContentContainer><div className={`${styles.student_course}`}>StudentCourse</div></ContentContainer>
+    <ContentContainer>
+      <CourseListContainer listName="Verified Course">
+        <VerifiedCourse courseName="Machine Learning" instructor="Alan Turing"/>
+        <VerifiedCourse courseName="Machine Learning" instructor="Alan Turing"/>
+        <VerifiedCourse courseName="Machine Learning" instructor="Alan Turing"/>
+        <VerifiedCourse courseName="Machine Learning" instructor="Alan Turing"/>
+      </CourseListContainer>
+    </ContentContainer>
   )
 }
 
