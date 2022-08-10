8bd13ee799d0749afad5c00176d733e99c68f662
Roihan <hroihan6@gmail.com>
2022-08-10 12:20:53 +0700

    Added sidebar link to correct content

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,8 +6,8 @@ import SidebarItem from '../../components/SidebarItem/SidebarItem'
 const Sidebar = (props) => {
     return (
     <SidebarContainer isShowSidebar={props.isShowSidebar}>
-        <SidebarItem label="Dashboard"><i class="fa-solid fa-gauge-high"></i></SidebarItem>
-        <SidebarItem label="Course"><i class="fa-solid fa-book-open"></i></SidebarItem>
+        <SidebarItem label="Dashboard" to="/student/dashboard"><i class="fa-solid fa-gauge-high"></i></SidebarItem>
+        <SidebarItem label="Course" to="/student/course"><i class="fa-solid fa-book-open"></i></SidebarItem>
     </SidebarContainer>
 )
 }
--- src/components/SidebarItem/SidebarItem.js
+++ src/components/SidebarItem/SidebarItem.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import styles from './SidebarItem.module.css';
 
 const SidebarItem = (props) => {
   return (
-    <li>
-        <div className={`${styles.icon}`}>
-            {props.children}
-        </div>
-        <span>{props.label}</span>
-    </li>
+    <Link to={props.to}>
+      <li>
+            <div className={`${styles.icon}`}>
+                {props.children}
+            </div>
+            <span>{props.label}</span>
+      </li>
+    </Link>
   )
 }
 
