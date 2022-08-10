33d207d8be7e369d519ad3d331cb3aa5cc60d17e
Roihan <hroihan6@gmail.com>
2022-08-10 10:06:59 +0700

    Added responsive enrolled course list prototype on student dashboard

--- src/layout/StudentDashboard/StudentDashboard.js
+++ src/layout/StudentDashboard/StudentDashboard.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import ContentContainer from '../../UI/ContentContainer/ContentContainer';
 import styles from './StudentDashboard.module.css';
+import ContentContainer from '../../UI/ContentContainer/ContentContainer';
 
 const StudentDashboard = () => {
   return (
-    <ContentContainer><div className={`${styles.student_dashboard}`}>StudentDashboard</div></ContentContainer>
+    <ContentContainer>
+      <div className={`${styles.enrolledCourse_container}`}>
+        <h1>Enrolled Course</h1>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </ContentContainer>
   )
 }
 
