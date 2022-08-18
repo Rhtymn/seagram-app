99af50567a2f19f65d0576bb1dd0bf9c18543a61
Roihan <hroihan6@gmail.com>
2022-08-18 12:52:03 +0700

    Added fetch course useEffect on verified course details

--- src/components/VerifiedCourseDetails/VerifiedCourseDetails.js
+++ src/components/VerifiedCourseDetails/VerifiedCourseDetails.js
@@ -2,32 +2,52 @@ import React from "react";
 import styles from "./VerifiedCourseDetails.module.css";
 import { useSelector } from "react-redux";
 import Button from "../../UI/Button/Button";
+import Spinner from "../Spinner/Spinner";
 
 const VerifiedCourseDetails = (props) => {
+  const SpinnerContainer = (
+    <div className={`${styles.spinner_container}`}>
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <div class="spinner-grow text-secondary" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+
+  const Content = (
+    <div className={`${styles.course_information}`}>
+      <div className="d-flex justify-content-between align-items-center">
+        {!props.isLoading && <h1>{props.state.title}</h1>}
+        {!props.isLoading && <span>{props.state.instructor}</span>}
+      </div>
+      <div className={`${styles.divider}`}></div>
+      <div className={`${styles.description}`}>
+        <div className={`${styles.description_title}`}>
+          <h2>Description</h2>
+        </div>
+        <div className={`${styles.description_text}`}>
+          {!props.isLoading && <p>{props.state.description}</p>}
+          {/* <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh
+        nisl condimentum id venenatis a condimentum vitae sapien
+        pellentesque. Vitae ultricies leo integer malesuada.
+      </p> */}
+        </div>
+      </div>
+      <Button>Enroll</Button>
+    </div>
+  );
+
   return (
     <>
-      <div className={`${styles.course_information}`}>
-        <div className="d-flex justify-content-between align-items-center">
-          <h1>{props.state.title}</h1>
-          <span>{props.state.instructor}</span>
-        </div>
-        <div className={`${styles.divider}`}></div>
-        <div className={`${styles.description}`}>
-          <div className={`${styles.description_title}`}>
-            <h2>Description</h2>
-          </div>
-          <div className={`${styles.description_text}`}>
-            <p>{props.state.description}</p>
-            {/* <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh
-              nisl condimentum id venenatis a condimentum vitae sapien
-              pellentesque. Vitae ultricies leo integer malesuada.
-            </p> */}
-          </div>
-        </div>
-        <Button>Enroll</Button>
-      </div>
+      {props.isLoading && SpinnerContainer}
+      {!props.isLoading && Content}
     </>
   );
 };
--- src/pages/CourseDetails/CourseDetails.js
+++ src/pages/CourseDetails/CourseDetails.js
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./CourseDetails.module.css";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, useParams } from "react-router-dom";
 import VerifiedCourseDetails from "../../components/VerifiedCourseDetails/VerifiedCourseDetails";
 
 const CourseDetails = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useParams();
   const navigate = useNavigate();
   const location = useLocation();
   const backHandler = () => {
-    navigate(`${location.state.back}`);
+    if (location.state.back) {
+      navigate(`${location.state.back}`);
+      return;
+    }
+    navigate("/login");
   };
 
-  if (location.state.type === "verified") {
-    return (
-      <div className={`${styles.course_details_container}`}>
-        <div
-          className={`${styles.course_details_actions}`}
-          onClick={backHandler}
-        >
-          <i class="fa-solid fa-arrow-left"></i>
-        </div>
-        <VerifiedCourseDetails state={location.state} />
+  useState(() => {
+    if (location.state) {
+      setIsLoading(false);
+      return;
+    }
+    const getCourse = async () => {
+      console.log("hello");
+      const response = await fetch(
+        `http://seagram-api.herokuapp.com/api/Courses/${params.courseId}`
+      );
+      const courseData = await response.json();
+      location.state = {
+        ...courseData,
+        type: `${courseData.isVerified}`,
+        instructor: "Robert",
+      };
+      setIsLoading(false);
+    };
+    getCourse();
+  }, []);
+
+  return (
+    <div className={`${styles.course_details_container}`}>
+      <div className={`${styles.course_details_actions}`} onClick={backHandler}>
+        <i class="fa-solid fa-arrow-left"></i>
       </div>
-    );
-  }
+      <VerifiedCourseDetails state={location.state} isLoading={isLoading} />
+    </div>
+  );
 };
 
 export default CourseDetails;
